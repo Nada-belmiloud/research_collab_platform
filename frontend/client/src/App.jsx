5055ca2d8e5b9def135cd0e9ee7b4183d85b60eb
LinaOuadah <lina.selma.ouadah@ensia.edu.dz>
2026-04-19 21:22:29 +0100

    RankingPageAdded

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
+import RankingPage from "./pages/RankingPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"       element={<HomePage />} />
+        <Route path="/"       element={<RankingPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="*"       element={<Navigate to="/" replace />} />
       </Routes>
